1c75288b34a451fe57b593a624a4f1e7b76d29f0
J Huang <i@jhuang.me>
2017-02-02 00:46:15 +0800

    feat(main): initial release

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import postcss from 'postcss'
+import valueParser from 'postcss-value-parser'
+import { flatMap } from 'lodash'
+
+const fontFamilySystemUIList = [
+  'system-ui',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  'Segoe UI',
+  'Roboto',
+  'Oxygen',
+  'Ubuntu',
+  'Cantarell',
+  'Fira Sans',
+  'Droid Sans',
+  'Helvetica Neue'
+]
+
+const parsedFontFamilySystemUIListTree = valueParser(fontFamilySystemUIList.join(', '))
+
+const transformFontFamilySystemUI = (nodes) => {
+  return flatMap(nodes, node => {
+    if (node.type === 'word' && node.value === 'system-ui') {
+      return parsedFontFamilySystemUIListTree
+    }
+    return node
+  })
+}
+
+const transform = () => (decl) => {
+  let tree
+  if (decl.type === 'decl') {
+    if (decl.prop === 'font-family' || decl.prop === 'font') {
+      tree = valueParser(decl.value)
+      tree.nodes = transformFontFamilySystemUI(tree.nodes)
+      decl.value = tree.toString()
+    }
+  }
+}
+
+export default postcss.plugin('postcss-font-family-system-ui', () => {
+  return (root) => {
+    root.walk(transform())
+  }
+})
---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+import { test } from 'tap'
+import fs from 'fs'
+import postcss from 'postcss'
+import plugin from '../src'
+
+function filename (name) { return `test/${name}.css` }
+function read (name) { return fs.readFileSync(name, 'utf8') }
+
+function compareFixtures (t, name, msg) {
+  const postcssOpts = { from: filename(`fixtures/${name}`) }
+  const actual = postcss().use(plugin()).process(read(postcssOpts.from), postcssOpts).css
+  const expected = read(filename(`fixtures/${name}.expected`))
+  fs.writeFileSync(filename(`fixtures/${name}.actual`), actual)
+  t.equal(actual, expected, msg)
+}
+
+test('font-variant', function (t) {
+  compareFixtures(t, 'font-family-system-ui', 'should transform font-family: system-ui')
+  t.end()
+})
