7a346ad691d271fbbb98051684fa9b6c246967c5
Huáng Jùnliàng <i@jhuang.me>
2017-11-09 15:33:02 +0800

    chore(test): use jest as test runner

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import { test } from 'tap'
+/* eslint-env jest */
 import fs from 'fs'
 import postcss from 'postcss'
 import plugin from '../src'
@@ -6,15 +6,13 @@ import plugin from '../src'
 function filename (name) { return `test/${name}.css` }
 function read (name) { return fs.readFileSync(name, 'utf8') }
 
-function compareFixtures (t, name, msg) {
-  const postcssOpts = { from: filename(`fixtures/${name}`) }
-  const actual = postcss().use(plugin()).process(read(postcssOpts.from), postcssOpts).css
-  const expected = read(filename(`fixtures/${name}.expected`))
-  fs.writeFileSync(filename(`fixtures/${name}.actual`), actual)
-  t.equal(actual, expected, msg)
-}
-
-test('font-family-system-ui', function (t) {
-  compareFixtures(t, 'font-family-system-ui', 'should transform font-family: system-ui')
-  t.end()
+describe('font-family-system-ui', function () {
+  it('should transform font-family: system-ui', (done) => {
+    const name = 'font-family-system-ui'
+    const postcssOpts = { from: filename(`fixtures/${name}`) }
+    const actual = postcss().use(plugin()).process(read(postcssOpts.from), postcssOpts).css
+    const expected = read(filename(`fixtures/${name}.expected`))
+    fs.writeFile(filename(`fixtures/${name}.actual`), actual, done)
+    expect(actual).toEqual(expected)
+  })
 })
