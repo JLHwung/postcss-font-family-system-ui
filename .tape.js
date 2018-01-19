6f0252388b8a4f2ca8a55dd8e7d1ea6ba83fa969
Jonathan Neal <jonathantneal@hotmail.com>
2018-01-19 12:43:41 -0500

    3.0.0

---
+++ .tape.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'postcss-system-ui-font': {
+		'basic': {
+			message: 'supports basic usage'
+		},
+		'basic:alt': {
+			message: 'supports { family } usage',
+			options: {
+				family: 'system-ui, Segoe UI, Roboto, Helvetica Neue'
+			}
+		}
+	}
+};
--- src/index.js
+++
@@ -1,3 +0,0 @@
-//eslint-disable-next-line
-require = require('@std/esm')(module)
-module.exports = require('./main.mjs').default
