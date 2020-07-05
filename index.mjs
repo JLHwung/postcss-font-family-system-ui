39036043062b201b80552e03b7b1661640103b22
Huáng Jùnliàng <jlhwung@gmail.com>
2020-07-05 15:55:04 -0400

    refactor: overhaul build scripts

--- index.mjs
+++ index.mjs
@@ -1,35 +1,41 @@
 // tooling
-import postcss from 'postcss';
-import getSystemUiFamily from './lib/get-system-ui-family.mjs';
-import browserslist from 'browserslist';
+import postcss from "postcss";
+import getSystemUiFamily from "./lib/get-system-ui-family.mjs";
+import browserslist from "browserslist";
 
 // plugin
-export default postcss.plugin('postcss-system-ui-font', opts => {
-
+export default postcss.plugin("postcss-system-ui-font", (opts) => {
 	return (root, result) => {
-
 		const { family, preserve = true, browsers } = Object(opts);
 		let systemUiFamily = family;
-		if (typeof family === 'string') {
+		if (typeof family === "string") {
 			systemUiFamily = family.trim().split(/\s*,\s*/);
 		} else if (!family) {
 			// browsers supported by the configuration
 			const supportedBrowsers = browserslist(browsers, {
-				path: result.root.source && result.root.source.input && result.root.source.input.file,
-				ignoreUnknownVersions: true
+				path:
+					result.root.source &&
+					result.root.source.input &&
+					result.root.source.input.file,
+				ignoreUnknownVersions: true,
 			});
 			systemUiFamily = getSystemUiFamily(supportedBrowsers, preserve);
 		}
 
 		// system-ui and fallbacks match
-		const whitespace = '[\\f\\n\\r\\x09\\x20]';
-		const systemUiMatch = new RegExp(`(^|,|${whitespace}+)(?:system-ui${whitespace}*)(?:,${whitespace}*(?:${systemUiFamily.join('|')})${whitespace}*)?(,|$)`, 'i');
+		const whitespace = "[\\f\\n\\r\\x09\\x20]";
+		const systemUiMatch = new RegExp(
+			`(^|,|${whitespace}+)(?:system-ui${whitespace}*)(?:,${whitespace}*(?:${systemUiFamily.join(
+				"|"
+			)})${whitespace}*)?(,|$)`,
+			"i"
+		);
 
 		// system-ui fallback replacement
-		const systemUiReplace = `$1${systemUiFamily.join(', ')}$2`;
+		const systemUiReplace = `$1${systemUiFamily.join(", ")}$2`;
 
 		// update font declarations to polyfill system-ui usage
-		root.walkDecls(fontPropertyMatch, decl => {
+		root.walkDecls(fontPropertyMatch, (decl) => {
 			decl.value = decl.value.replace(systemUiMatch, systemUiReplace);
 		});
 	};
