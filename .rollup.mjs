5860266e18c6df72bdd17cb76d975bc979020c65
Huáng Jùnliàng <jlhwung@gmail.com>
2020-07-05 19:15:46 -0400

    chore: rollup-plugin-babel has named exports

--- .rollup.mjs
+++ .rollup.mjs
@@ -5,13 +5,8 @@ export default {
 	input: "index.mjs",
 	output: { file: "index.bundle.cjs", format: "cjs" },
 	plugins: [
-		babel({
-			presets: [
-				[
-					"@babel/preset-env",
-					{ modules: false, targets: { node: 6 }, loose: true },
-				],
-			],
+		babel.default({
+			presets: [["@babel/preset-env", { targets: { node: 6 }, loose: true }]],
 			plugins: [["@babel/plugin-transform-for-of", { assumeArray: true }]],
 			babelHelpers: "bundled",
 		}),
