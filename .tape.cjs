186632cfba499a3e684236700539e372074a404a
Huáng Jùnliàng <jlhwung@gmail.com>
2020-07-05 17:36:22 -0400

    fix: preserve: false should not remove system-ui when it is no-op (#284)

--- .tape.cjs
+++ .tape.cjs
@@ -1,38 +1,52 @@
 module.exports = {
-		'basic': {
-			message: 'supports basic usage'
+	basic: {
+		message: "supports basic usage",
+	},
+	"basic:family": {
+		message: "supports { family } usage",
+		options: {
+			family: "system-ui, Segoe UI, Roboto, Helvetica Neue",
 		},
-		'basic:family': {
-			message: 'supports { family } usage',
-			options: {
-				family: 'system-ui, Segoe UI, Roboto, Helvetica Neue'
-			}
+	},
+	"basic:browser:chrome_60": {
+		message: "supports simple { browsers } usage",
+		options: {
+			browsers: ["Chrome 60"],
 		},
-		'basic:browser:chrome_60': {
-			message: 'supports simple { browsers } usage',
-			options: {
-				browsers: ["Chrome 60"]
-			}
+	},
+	"basic:browser:chrome_60": {
+		message: "preserve: false is no-op when browsers are modern",
+		options: {
+			browsers: ["Chrome 60"],
+			preserve: false,
 		},
-		'basic:browser:whole_coverage': {
-			message: 'supports complex { browsers } usage',
-			options: {
-				browsers: ["Safari 9", "Safari 10", "IE 11", "Chrome 55", "Chrome 56", "Firefox 57"]
-			}
+	},
+	"basic:browser:whole_coverage": {
+		message: "supports complex { browsers } usage",
+		options: {
+			browsers: [
+				"Safari 9",
+				"Safari 10",
+				"IE 11",
+				"Chrome 55",
+				"Chrome 56",
+				"Firefox 57",
+			],
 		},
-		'basic:browser:edged_version': {
-			message: 'supports browsers usage when caniuse is out-of-dated',
-			options: {
-				browsers: ["Chrome 1048576"]
-			}
+	},
+	"basic:browser:edged_version": {
+		message: "supports browsers usage when caniuse is out-of-dated",
+		options: {
+			browsers: ["Chrome 1048576"],
 		},
-		'basic:not_preserve': {
-			message: 'supports simple { preserve } usage',
-			options: {
-				preserve: false,
-			}
+	},
+	"basic:not_preserve": {
+		message: "supports simple { preserve } usage",
+		options: {
+			preserve: false,
 		},
-		'custom-properties': {
-			message: 'supports custom-properties usage'
-		}
+	},
+	"custom-properties": {
+		message: "supports custom-properties usage",
+	},
 };
