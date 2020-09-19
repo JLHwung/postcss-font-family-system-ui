4c46dc6ae4b58b270d88315dd42ab126ffe43b5f
Huáng Jùnliàng <jlhwung@gmail.com>
2020-09-18 21:07:21 -0400

    chore: build cjs and es module via rollup

--- .rollup.mjs
+++ .rollup.mjs
@@ -2,7 +2,10 @@ import babel from "@rollup/plugin-babel";
 
 export default {
 	input: "index.mjs",
-	output: { file: "index.bundle.cjs", format: "cjs", exports: "default" },
+	output: [
+		{ file: "dist/index.bundle.cjs", format: "cjs", exports: "default" },
+		{ file: "dist/index.bundle.mjs", format: "es" }
+	],
 	external: ["browserslist", "caniuse-lite"],
 	plugins: [
 		babel.default({
